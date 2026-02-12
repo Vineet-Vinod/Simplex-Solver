e9d3a770943a081a58db17fe54206a712bebdd09
Vineet-V <vineetv314@gmail.com>
2026-02-11 22:00:53 -0500

    Init implementation

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-def main():
-    print("Hello from simplex-solver!")
+from simplex import Simplex
+
+
+def main(filename: str) -> None:
+    """
+    Expect filename to contain lines with comma separated values
+    representing entries in the matrix - not trying to convert
+    word problems into LPs (idt this is possible programmatically),
+    nor trying to convert eqns in set notation to simplex standard form
+    as it involves parsing + getting to the form which is more an exercise
+    in boilerplate programming than actual logic which is not the point here
+    (AI can do that really well and we can check its work)
+    """
+    with open(filename, "r") as file:
+        eqns = [line.strip().split(",") for line in file.readlines()]
+    solver = Simplex(eqns)
+    solver.solve()
+    print(solver.solution())
 
 
 if __name__ == "__main__":
-    main()
+    main("problem.txt")
---
+++ simplex.py
@@ -0,0 +1,60 @@
+from typing import List
+import sys
+
+class Simplex():
+    def __init__(self, eqns: List[List[str]]):
+        try:
+            self.mat: List[List[float]] = [[float(entry) for entry in eqn] for eqn in eqns]
+        except:
+            print('Could not parse the matrix. Please check and try again')
+            sys.exit(1)
+        
+        self.eqns: int = len(self.mat)
+        self.vars: int = len(self.mat[0])
+
+    def solve(self) -> None:
+        """
+        3 cases
+        1. Feasible and bounded
+        2. Feasible and unbounded - check at the start of every loop and report if unbounded
+        3. Infeasible - cannot bring to standard form as standard form gives A solution - don't need to handle
+        """
+        while any(val > 0 for val in self.mat[0][:-1]): # Can keep simplexing
+            ma, argma = self.mat[0][0], 0
+            for xi, val in enumerate(self.mat[0][1:-1], 1):
+                if val > ma:
+                    ma = val
+                    argma = xi
+            
+            if all(row[argma] < 0 for row in self.mat[1:]): # Unbounded check
+                print("Unbounded LP - no solution")
+                break
+
+            mi, argmi = self.mat[1][-1] / self.mat[1][argma], 1
+            for yi in range(2, self.eqns):
+                if mi > self.mat[yi][-1] / self.mat[yi][argma]:
+                    mi = self.mat[yi][-1] / self.mat[yi][argma]
+                    argmi = yi
+            
+            print(f"Pivoting around ({argma}, {argmi})")
+            div = self.mat[argmi][argma]
+            for i in range(self.vars): self.mat[argmi][i] /= div
+            for i in range(self.eqns):
+                if i != argmi:
+                    mult = self.mat[i][argma] / self.mat[argmi][argma]
+                    for j in range(self.vars):
+                        self.mat[i][j] -= mult * self.mat[argmi][j]
+
+    def solution(self) -> str:
+        soln: List[float] = []
+        for xi in range(self.vars - 1):
+            if sum(row[xi] == 0.0 for row in self.mat) == self.eqns - 1:
+                print(f"x{xi} is a basic variable")
+                for row in self.mat:
+                    if row[xi]:
+                        soln.append(str(row[-1] / row[xi]))
+            else:
+                print(f"x{xi} is a free variable")
+                soln.append("0")
+        
+        return f"Solution is: ({", ".join(soln)}) and the objective value is {-self.mat[0][-1]}"
